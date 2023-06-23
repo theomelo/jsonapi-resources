6bb3c582e5140ee7e145da7f3499efb117fb0748
Theo Melo <9499829+theomelo@users.noreply.github.com>
2023-06-23 16:00:03 -0700

    feat: add relationships meta

--- lib/jsonapi/resource_serializer.rb
+++ lib/jsonapi/resource_serializer.rb
@@ -425,22 +425,31 @@ module JSONAPI
 
     def link_object_to_one(source, relationship, include_linkage)
       include_linkage = include_linkage | @always_include_to_one_linkage_data | relationship.always_include_linkage_data
+      include_meta = relationship.options.fetch(:meta, nil).present?
       link_object_hash = {}
       link_object_hash[:links] = {}
       link_object_hash[:links][:self] = self_link(source, relationship)
       link_object_hash[:links][:related] = related_link(source, relationship)
       link_object_hash[:data] = to_one_linkage(source, relationship) if include_linkage
+      link_object_hash[:meta] = relationship_meta(source, relationship) if include_meta
       link_object_hash
     end
 
     def link_object_to_many(source, relationship, include_linkage)
       include_linkage = include_linkage | relationship.always_include_linkage_data
+      include_meta = relationship.options.fetch(:meta, nil).present?
       link_object_hash = {}
       link_object_hash[:links] = {}
       link_object_hash[:links][:self] = self_link(source, relationship)
       link_object_hash[:links][:related] = related_link(source, relationship)
       link_object_hash[:data] = to_many_linkage(source, relationship) if include_linkage
+      link_object_hash[:meta] = relationship_meta(source, relationship) if include_meta
       link_object_hash
+    end
+
+    def relationship_meta(source, relationship)
+      meta = relationship.options.fetch(:meta, {})
+      meta.is_a?(Proc) ? meta.(source) : meta
     end
 
     def link_object(source, relationship, include_linkage = false)
